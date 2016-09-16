014cadc1d01844b4a16352b223dc226dadca4086
worron <worrongm@gmail.com>
2016-09-16 19:38:29 +0300

    Add menu switcher to quick launcher

--- float/qlaunch.lua
+++ float/qlaunch.lua
@@ -8,14 +8,28 @@
 -----------------------------------------------------------------------------------------------------------------------
 local table = table
 local unpack = unpack
+local string = string
 
 local awful = require("awful")
 local redflat = require("redflat")
+local beautiful = require("beautiful")
 
 -- Initialize tables and vars for module
 -----------------------------------------------------------------------------------------------------------------------
 local qlaunch = { hotkeys = {}, history = {} }
-local sw = redflat.float.appswitcher
+local swpack = { apps = {}, menu = {} }
+
+-- Generate default theme vars
+-----------------------------------------------------------------------------------------------------------------------
+local function default_style()
+	local style = {
+		sw_type  = "menu",
+		switcher = { apps = {}, menu = { width = 800 } },
+	}
+
+	return redflat.util.table.merge(style, redflat.util.check(beautiful, "float.qlaunch") or {})
+end
+
 
 -- Support functions
 -----------------------------------------------------------------------------------------------------------------------
@@ -41,12 +55,47 @@ local function build_filter(app)
 	end
 end
 
+-- redflat appswitcher
+function swpack.apps:init(style)
+	self.widget = redflat.float.appswitcher
+	self.widget:init()
+end
+
+function swpack.apps:activate(app, clist)
+	self.widget:show({ filter = build_filter(app) })
+end
+
+-- redflat menu switcher
+function swpack.menu:init(style)
+	self.style = style
+end
+
+function swpack.menu:activate(app, clist)
+	local items = {}
+	for _, c in ipairs(clist) do
+		local client_tags = ""
+		for _, t in ipairs(c:tags()) do client_tags = client_tags .. " " .. string.upper(t.name) end
+		table.insert(items, { string.format("[%s ] %s", client_tags, c.name), function() focus_and_raise(c) end })
+	end
+
+	self.widget = redflat.menu({ theme = self.style, items = items })
+	redflat.util.placement.centered(self.widget.wibox, nil, screen[mouse.screen].workarea)
+	self.widget:show({ coords = self.widget.wibox:geometry() })
+
+	collectgarbage() -- FIX THIS !!!
+end
+
+
 -- Initialize widget
 -----------------------------------------------------------------------------------------------------------------------
-function qlaunch:init(args)
+function qlaunch:init(args, style)
 	local args = args or {}
 	local modkeys = args.modkeys or { "Mod1" }
 	self.apps = args.apps or {}
+
+	local style = redflat.util.table.merge(default_style(), style or {})
+	self.switcher = swpack[style.sw_type]
+	self.switcher:init(style.switcher[style.sw_type])
 
 	for app, data in pairs(self.apps) do
 		self.hotkeys = awful.util.table.join(
@@ -67,7 +116,7 @@ function qlaunch:run_or_raise(app)
 		focus_and_raise(clients[1])
 	else
 		if awful.util.table.hasitem(clients, client.focus) then
-			sw:show({ filter = build_filter(app) })
+			self.switcher:activate(app, clients)
 		else
 			local last = awful.util.table.hasitem(clients, self.history[app])
 			if last then
