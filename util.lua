9e4191681cbdcc02cd9f409bc685173af095fe0b
worron <worrongm@gmail.com>
2017-03-17 00:59:56 +0300

    Split util module: placement functions

--- util.lua
+++ util.lua
@@ -30,6 +30,7 @@ util.cairo = require("redflat.newutil.cairo")
 util.key = require("redflat.newutil.key")
 util.desktop = require("redflat.newutil.desktop")
 util.client = require("redflat.newutil.client")
+util.placement = require("redflat.newutil.placement")
 
 -----------------------------------------------------------------------------------------------------------------------
 function util.placeholder(args)
@@ -289,57 +290,57 @@ end
 
 -- Placement utilits
 -----------------------------------------------------------------------------------------------------------------------
-local direction = { x = "width", y = "height" }
+-- local direction = { x = "width", y = "height" }
 
-function util.placement.add_gap(geometry, gap)
-	return {
-		x = geometry.x + gap,
-		y = geometry.y + gap,
-		width = geometry.width - 2 * gap,
-		height = geometry.height - 2 * gap
-	}
-end
+-- function util.placement.add_gap(geometry, gap)
+-- 	return {
+-- 		x = geometry.x + gap,
+-- 		y = geometry.y + gap,
+-- 		width = geometry.width - 2 * gap,
+-- 		height = geometry.height - 2 * gap
+-- 	}
+-- end
 
-function util.placement.no_offscreen(object, gap, area)
-	local geometry = object:geometry()
-	local border = object.border_width
+-- function util.placement.no_offscreen(object, gap, area)
+-- 	local geometry = object:geometry()
+-- 	local border = object.border_width
 
-	local screen_idx = object.screen or awful.screen.getbycoord(geometry.x, geometry.y)
-	local area = area or screen[screen_idx].workarea
-	if gap then area = util.placement.add_gap(area, gap) end
+-- 	local screen_idx = object.screen or awful.screen.getbycoord(geometry.x, geometry.y)
+-- 	local area = area or screen[screen_idx].workarea
+-- 	if gap then area = util.placement.add_gap(area, gap) end
 
-	for coord, dim in pairs(direction) do
-		if geometry[coord] + geometry[dim] + 2 * border > area[coord] + area[dim] then
-			geometry[coord] = area[coord] + area[dim] - geometry[dim] - 2*border
-		elseif geometry[coord] < area[coord] then
-			geometry[coord] = area[coord]
-		end
-	end
+-- 	for coord, dim in pairs(direction) do
+-- 		if geometry[coord] + geometry[dim] + 2 * border > area[coord] + area[dim] then
+-- 			geometry[coord] = area[coord] + area[dim] - geometry[dim] - 2*border
+-- 		elseif geometry[coord] < area[coord] then
+-- 			geometry[coord] = area[coord]
+-- 		end
+-- 	end
 
-	object:geometry(geometry)
-end
+-- 	object:geometry(geometry)
+-- end
 
-local function centered_base(is_h, is_v)
-	return function(object, gap, area)
-		local geometry = object:geometry()
-		local new_geometry = {}
+-- local function centered_base(is_h, is_v)
+-- 	return function(object, gap, area)
+-- 		local geometry = object:geometry()
+-- 		local new_geometry = {}
 
-		local screen_idx = object.screen or awful.screen.getbycoord(geometry.x, geometry.y)
-		local area = area or screen[screen_idx].geometry
-		if gap then area = util.placement.add_gap(area, gap) end
+-- 		local screen_idx = object.screen or awful.screen.getbycoord(geometry.x, geometry.y)
+-- 		local area = area or screen[screen_idx].geometry
+-- 		if gap then area = util.placement.add_gap(area, gap) end
 
-		if is_h then new_geometry.x = area.x + (area.width - geometry.width) / 2 - object.border_width end
-		if is_v then new_geometry.y = area.y + (area.height - geometry.height) / 2 - object.border_width end
+-- 		if is_h then new_geometry.x = area.x + (area.width - geometry.width) / 2 - object.border_width end
+-- 		if is_v then new_geometry.y = area.y + (area.height - geometry.height) / 2 - object.border_width end
 
-		return object:geometry(new_geometry)
-	end
-end
+-- 		return object:geometry(new_geometry)
+-- 	end
+-- end
 
-util.placement.centered = setmetatable({}, {
-	__call = function(_, ...) return centered_base(true, true)(...) end
-})
-util.placement.centered.horizontal = centered_base(true, false)
-util.placement.centered.vertical = centered_base(false, true)
+-- util.placement.centered = setmetatable({}, {
+-- 	__call = function(_, ...) return centered_base(true, true)(...) end
+-- })
+-- util.placement.centered.horizontal = centered_base(true, false)
+-- util.placement.centered.vertical = centered_base(false, true)
 
 -- Client utilits
 -----------------------------------------------------------------------------------------------------------------------
