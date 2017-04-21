54665848d899625f6dad4bd05e5de51fcfe1d8bc
worron <worrongm@gmail.com>
2017-04-22 01:05:24 +0300

    Ignore 'Unknown' modificator during key check
    Possibly have some connection with #2.

--- util/key.lua
+++ util/key.lua
@@ -23,6 +23,8 @@ end
 -- Check if redflat raw key matched with awful prompt key
 ------------------------------------------------------------
 function key.match_prompt(rawkey, mod, key)
+	for m, _ in pairs(mod) do if m == "Unknown" then mod["Unknown"] = nil; break end end
+
 	local modcheck = true
 	local count = 0
 
@@ -37,6 +39,8 @@ end
 -- Check if redflat raw key matched with awful prompt key
 ------------------------------------------------------------
 function key.match_grabber(rawkey, mod, key)
+	for i, m in ipairs(mod) do if m == "Unknown" then table.remove(mod, i); break end end
+
 	local modcheck = #mod == #rawkey[1]
 	for _, v in ipairs(mod) do modcheck = modcheck and awful.util.table.hasitem(rawkey[1], v) end
 	return modcheck and key:lower() == rawkey[2]:lower()
