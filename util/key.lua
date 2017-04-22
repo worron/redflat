26f4ae76fe87f55a0f8730f5195751f03776265b
worron <worrongm@gmail.com>
2017-04-22 21:42:42 +0300

    Add optional list of ignored modificators
    Half solution for #2 and similar.

--- util/key.lua
+++ util/key.lua
@@ -5,6 +5,8 @@ local awful = require("awful")
 
 local key = {}
 
+key.ignored_mod = { "Unknown", "Mod2" }
+
 -- Functions
 -----------------------------------------------------------------------------------------------------------------------
 
@@ -22,8 +24,13 @@ end
 
 -- Check if redflat raw key matched with awful prompt key
 ------------------------------------------------------------
-function key.match_prompt(rawkey, mod, key)
-	for m, _ in pairs(mod) do if m == "Unknown" then mod["Unknown"] = nil; break end end
+function key.match_prompt(rawkey, mod, _key)
+	for m, _ in pairs(mod) do
+		if awful.util.table.hasitem(key.ignored_mod, m) then
+			mod[m] = nil
+			break
+		end
+	end
 
 	local modcheck = true
 	local count = 0
@@ -33,17 +40,22 @@ function key.match_prompt(rawkey, mod, key)
 		count = count + 1
 	end
 
-	return #rawkey[1] == count and modcheck and key:lower() == rawkey[2]:lower()
+	return #rawkey[1] == count and modcheck and _key:lower() == rawkey[2]:lower()
 end
 
 -- Check if redflat raw key matched with awful prompt key
 ------------------------------------------------------------
-function key.match_grabber(rawkey, mod, key)
-	for i, m in ipairs(mod) do if m == "Unknown" then table.remove(mod, i); break end end
+function key.match_grabber(rawkey, mod, _key)
+	for i, m in ipairs(mod) do
+		if awful.util.table.hasitem(key.ignored_mod, m) then
+			table.remove(mod, i)
+			break
+		end
+	end
 
 	local modcheck = #mod == #rawkey[1]
 	for _, v in ipairs(mod) do modcheck = modcheck and awful.util.table.hasitem(rawkey[1], v) end
-	return modcheck and key:lower() == rawkey[2]:lower()
+	return modcheck and _key:lower() == rawkey[2]:lower()
 end
 
 
