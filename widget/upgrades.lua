1d650194c3dfcb5233b51db1824122c317646943
worron <worrongm@gmail.com>
2017-07-01 23:44:24 +0300

    Add custom command for upgrades widget

--- widget/upgrades.lua
+++ widget/upgrades.lua
@@ -41,12 +41,14 @@ end
 -- @param style Table containing colors and geometry parameters for all elemets
 -- @param update_timeout Update interval
 -----------------------------------------------------------------------------------------------------------------------
-function upgrades.new(update_timeout, style)
+function upgrades.new(args, style)
 
 	-- Initialize vars
 	--------------------------------------------------------------------------------
 	local object = {}
-	local update_timeout = update_timeout or 3600
+	local args = args or {}
+	local update_timeout = args.update_timeout or 3600
+	local command = args.command or [[bash -c 'apt-get upgrade -s | grep -P "^\d+ upgraded" | cut -d " " -f1']]
 	local force_notify = false
 	local style = redutil.table.merge(default_style(), style or {})
 
@@ -61,7 +63,7 @@ function upgrades.new(update_timeout, style)
 	-- Update info function
 	--------------------------------------------------------------------------------
 	local function update_count(output)
-		local c = string.match(output, "(%d+)%supgraded")
+		local c = string.match(output, "(%d+)")
 		object.tp:set_text(c .. " updates")
 
 		local color = tonumber(c) > 0 and style.color.main or style.color.icon
@@ -75,7 +77,7 @@ function upgrades.new(update_timeout, style)
 	function object.update(args)
 		local args = args or {}
 		force_notify = args.is_force
-		awful.spawn.easy_async("apt-get --just-print upgrade", update_count)
+		awful.spawn.easy_async(command, update_count)
 	end
 
 	-- Set update timer
