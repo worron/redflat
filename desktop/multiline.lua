fab2a19dd92709444a8e72145d48df3f496e511d
worron <worrongm@gmail.com>
2018-12-28 20:47:36 +0300

    Add icon to multi line desktop widget

--- desktop/multiline.lua
+++ desktop/multiline.lua
@@ -9,12 +9,14 @@
 -----------------------------------------------------------------------------------------------------------------------
 local setmetatable = setmetatable
 local string = string
+local unpack = unpack
 
 local wibox = require("wibox")
 local beautiful = require("beautiful")
 local timer = require("gears.timer")
 
 local redutil = require("redflat.util")
+local svgbox = require("redflat.gauge.svgbox")
 local lines = require("redflat.desktop.common.pack.lines")
 
 -- Initialize tables for module
@@ -26,7 +28,8 @@ local dashpack = { mt = {} }
 local function default_style()
 	local style = {
 		show_text = false,
-		lines   = {},
+		icon      = { image = nil, margin = { 0, 0, 0, 0 } },
+		lines     = {},
 		digit_num = 3,
 		unit      = { { "B", -1 }, { "KB", 1024 }, { "MB", 1024^2 }, { "GB", 1024^3 } },
 		color     = { main = "#b1222b", wibox = "#161616", gray = "#404040" }
@@ -56,7 +59,18 @@ function dashpack.new(args, geometry, style)
 	-- initialize progressbar lines
 	local lines_style = redutil.table.merge(style.lines, { color = style.color })
 	local pack = lines(#args.sensors, lines_style)
-	dwidget.wibox:set_widget(pack.layout)
+
+	if style.icon.image then
+		dwidget.icon = svgbox(style.icon.image)
+		dwidget.icon:set_color(style.color.gray)
+
+		local align = wibox.layout.align.horizontal()
+		align:set_middle(pack.layout)
+		align:set_left(wibox.container.margin(dwidget.icon, unpack(style.icon.margin)))
+		dwidget.wibox:set_widget(align)
+	else
+		dwidget.wibox:set_widget(pack.layout)
+	end
 
 	for i, name in ipairs(args.names) do
 		pack:set_label(string.upper(name), i)
@@ -65,10 +79,13 @@ function dashpack.new(args, geometry, style)
 	-- Update info function
 	--------------------------------------------------------------------------------
 	local function update()
+		local ico_alert = false
 		for i, sens in ipairs(args.sensors) do
 			local state = sens.meter_function(sens.args)
-			local text_color = sens.crit and state[1] > sens.crit and style.color.main or style.color.gray
+			local alert = sens.crit and state[1] > sens.crit
+			local text_color = alert and style.color.main or style.color.gray
 
+			ico_alert = ico_alert or alert
 			pack:set_values(state[1] / sens.maxm, i)
 			pack:set_label_color(text_color, i)
 
@@ -76,6 +93,10 @@ function dashpack.new(args, geometry, style)
 				pack:set_text(redutil.text.dformat(state[2] or state[1], style.unit, style.digit_num), i)
 				pack:set_text_color(text_color, i)
 			end
+		end
+
+		if style.icon.image then
+			dwidget.icon:set_color(ico_alert and style.color.main or style.color.gray)
 		end
 	end
 
