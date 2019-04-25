eaab3c49280c11beb13a2efa535fd0b5d48d5950
worron <worrongm@gmail.com>
2019-04-25 03:05:33 +0300

    Add floating window control helper features and fixes

--- float/control.lua
+++ float/control.lua
@@ -34,10 +34,13 @@ local function default_style()
 		shape         = nil,
 		steps         = { 1, 10, 20, 50 },
 		default_step  = 2,
-		margin        = { icon = { title = { 10, 10, 2, 2 }, state = { 10, 10, 2, 2 } } },
+		is_resizing   = false,
+		onscreen      = true,
+		margin        = { icon = { onscreen = { 10, 10, 2, 2 }, mode = { 10, 10, 2, 2 } } },
 		icon          = {
-			unknown  = redutil.base.placeholder({ txt = "?" }),
-			title  = redutil.base.placeholder({ txt = "?" }),
+			move     = redutil.base.placeholder({ txt = "M" }),
+			resize   = redutil.base.placeholder({ txt = "R" }),
+			onscreen = redutil.base.placeholder({ txt = "X" }),
 		},
 		color         = { border = "#575757", text = "#aaaaaa", main = "#b1222b", wibox = "#202020",
 		                  gray = "#575757", icon = "#a0a0a0" },
@@ -61,6 +64,30 @@ control.keys.control = {
 		{ "Mod4" }, "a", function() control:resize(true) end,
 		{ description = "Decrease window size", group = "Window control" }
 	},
+	{
+		{ "Mod4" }, "l", function() control:direction_action("right") end,
+		{ description = "Move/resize window to right", group = "Window control" }
+	},
+	{
+		{ "Mod4" }, "j", function() control:direction_action("left") end,
+		{ description = "Move/resize window to left", group = "Window control" }
+	},
+	{
+		{ "Mod4" }, "k", function() control:direction_action("bottom") end,
+		{ description = "Move/resize window to bottom", group = "Window control" }
+	},
+	{
+		{ "Mod4" }, "i", function() control:direction_action("top") end,
+		{ description = "Move/resize window to top", group = "Window control" }
+	},
+	{
+		{ "Mod4" }, "n", function() control:switch_mode() end,
+		{ description = "Switch moving/resizing mode", group = "Mode" }
+	},
+	{
+		{ "Mod4" }, "s", function() control:switch_onscreen() end,
+		{ description = "Switch off screen check", group = "Mode" }
+	},
 }
 control.keys.action = {
 	{
@@ -78,10 +105,23 @@ control.keys.all = awful.util.table.join(control.keys.control, control.keys.acti
 control._fake_keys = {
 	{
 		{}, "N", nil,
-		{ description = "Select nove/resize step", group = "Mode",
+		{ description = "Select move/resize step", group = "Mode",
 		  keyset = { "1", "2", "3", "4", "5", "6", "7", "8", "9" } }
 	},
 }
+
+
+-- Support function
+-----------------------------------------------------------------------------------------------------------------------
+local function control_off_screen(window)
+	local wa = screen[mouse.screen].workarea
+	local newg = window:geometry()
+
+	if newg.width > wa.width then window:geometry({ width = wa.width, x = wa.x }) end
+	if newg.height > wa.height then window:geometry({ height = wa.height, y = wa.y }) end
+
+	redutil.placement.no_offscreen(window, nil, wa)
+end
 
 -- Initialize widget
 -----------------------------------------------------------------------------------------------------------------------
@@ -93,6 +133,9 @@ function control:init()
 	self.style = style
 	self.client = nil
 	self.step = style.steps[style.default_step]
+
+	self.is_resizing = style.is_resizing
+	self.onscreen = style.onscreen
 
 	-- Create floating wibox for top widget
 	--------------------------------------------------------------------------------
@@ -112,16 +155,16 @@ function control:init()
 	self.label:set_align("center")
 	self.label:set_font(style.font)
 
-	local title_icon = svgbox(self.style.icon.title)
-	title_icon:set_color(self.style.color.icon)
+	self.onscreen_icon = svgbox(self.style.icon.onscreen)
+	self.onscreen_icon:set_color(self.onscreen and self.style.color.main or self.style.color.icon)
 
-	self.state_icon = svgbox()
+	self.mode_icon = svgbox(self.is_resizing and self.style.icon.resize or self.style.icon.move)
+	self.mode_icon:set_color(self.style.color.icon)
 
-	--self.wibox:set_widget(self.label)
 	self.wibox:setup({
-		wibox.container.margin(title_icon, unpack(self.style.margin.icon.title)),
+		wibox.container.margin(self.onscreen_icon, unpack(self.style.margin.icon.onscreen)),
 		self.label,
-		wibox.container.margin(self.state_icon, unpack(self.style.margin.icon.state)),
+		wibox.container.margin(self.mode_icon, unpack(self.style.margin.icon.mode)),
 		layout = wibox.layout.align.horizontal
 	})
 
@@ -153,6 +196,9 @@ end
 function control:center()
 	if not self.client then return end
 	redutil.placement.centered(self.client, nil, mouse.screen.workarea)
+
+	if self.onscreen then control_off_screen(self.client) end
+	self:update()
 end
 
 -- Change window size
@@ -164,6 +210,39 @@ function control:resize(is_shrinking)
 	local d = self.step * (is_shrinking and -1 or 1)
 
 	self.client:geometry({ x = g.x - d, y = g.y - d, width = g.width + 2 * d, height = g.height + 2 * d })
+	if self.onscreen then control_off_screen(self.client) end
+	self:update()
+end
+
+
+-- Move/resize by direction
+--------------------------------------------------------------------------------
+function control:direction_action(direction)
+	if not self.client then return end
+
+	local g = self.client:geometry()
+
+	if self.is_resizing then
+		if direction == "left" then
+			self.client:geometry({ x = g.x + self.step, width = g.width - 2 * self.step  })
+		elseif direction == "right" then
+			self.client:geometry({ x = g.x - self.step, width = g.width + 2 * self.step  })
+		elseif direction == "top" then
+			self.client:geometry({ y = g.y - self.step, height = g.height + 2 * self.step  })
+		elseif direction == "bottom" then
+			self.client:geometry({ y = g.y + self.step, height = g.height - 2 * self.step  })
+		end
+	else
+		local d = self.step * ((direction == "left" or direction == "top") and -1 or 1)
+
+		if direction == "left" or direction == "right" then
+			self.client:geometry({ x = g.x + d })
+		else
+			self.client:geometry({ y = g.y + d })
+		end
+	end
+
+	if self.onscreen then control_off_screen(self.client) end
 	self:update()
 end
 
@@ -178,9 +257,9 @@ function control:update()
 
 	local g = self.client:geometry()
 	local size_label = string.format("%sx%s", g.width, g.height)
-	--
+
 	self.label:set_markup(string.format(
-		'<span color="%s">%s</span><span color="%s">[%d]</span>',
+		'<span color="%s">%s</span><span color="%s"> [%d]</span>',
 		self.style.color.text, size_label, self.style.color.gray, self.step
 	))
 end
@@ -190,6 +269,25 @@ end
 function control:choose_step(index)
 	if self.style.steps[index] then self.step = self.style.steps[index] end
 	self:update()
+end
+
+-- Switch move/resize mode
+--------------------------------------------------------------------------------
+function control:switch_mode()
+	self.is_resizing = not self.is_resizing
+	self.mode_icon:set_image(self.is_resizing and self.style.icon.resize or self.style.icon.move)
+end
+
+-- Switch onscreen mode
+--------------------------------------------------------------------------------
+function control:switch_onscreen()
+	self.onscreen = not self.onscreen
+	self.onscreen_icon:set_color(self.onscreen and self.style.color.main or self.style.color.icon)
+
+	if self.onscreen then
+		control_off_screen(self.client)
+		self:update()
+	end
 end
 
 -- Show
@@ -219,7 +317,7 @@ function control:show()
 		self.wibox.visible = true
 		awful.keygrabber.run(self.keygrabber)
 		redtip:set_pack(
-			"Titlebar", self.tip, self.style.keytip.column, self.style.keytip.geometry,
+			"Floating window", self.tip, self.style.keytip.column, self.style.keytip.geometry,
 			function() self:hide() end
 		)
 	end
